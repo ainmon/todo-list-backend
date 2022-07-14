3136a79812f0f18a93186a733624fbb379c13ba9
Adam <inmon.adam@yahoo.com>
2022-07-14 14:06:29 -0500

    final push

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,8 @@ Todo.create(task: "Make dinner", completed: false, task_id: 1)
 
 Todo.create(task: "Run a 5 minute mile", completed: false)
 
+Category.create(category_name: "Fitness")
+Category.create(category_name: "Education")
+Category.create(category_name: "Business")
+
 puts "✅ Done seeding!"
